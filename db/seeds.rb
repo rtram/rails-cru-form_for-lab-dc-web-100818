0bc9de69e481d4c07521297fe7044414e512025b
Robin Tram <robin.tram@gmail.com>
2018-11-05 12:38:09 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: "Robin Tram", bio: "I am an Artist")
+Artist.create(name: "Sarah Tram", bio: "I am actually an Artist")
+
+Genre.create(name: "Hip-Hop")
+Genre.create(name: "Pop")
+
+Song.create(name: "Song 1", artist_id: 1, genre_id: 1)
+Song.create(name: "Song 1", artist_id: 2, genre_id: 2)
